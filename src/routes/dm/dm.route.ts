23c2003bb71b4bf606e833faa9423c9f1990bc43
Sulton <sultonalidrus5@gmail.com>
2024-12-19 13:58:41 +0700

    ref : add auth middleware

--- src/routes/dm/dm.route.ts
+++ src/routes/dm/dm.route.ts
@@ -6,28 +6,30 @@ import { authMiddleware } from "@/middleware/auth.middleware";
 
 export const dm = new Hono<Env>().basePath("/dm");
 
-dm.post("/", validationCreateDm, authMiddleware, async (c) => {
-  const { id } = c.get("user");
-  const data = c.req.valid("json");
-  const chat = await createChatPersonal({
-    content: data.content,
-    userId: id,
-    other: data.other,
+dm.use(authMiddleware)
+  .post("/", validationCreateDm, async (c) => {
+    const { id } = c.get("user");
+    const data = c.req.valid("json");
+    const chat = await createChatPersonal({
+      content: data.content,
+      userId: id,
+      other: data.other,
+    });
+
+    if (!chat) {
+      return c.json(
+        {
+          message: "Error send message",
+        },
+        500
+      );
+    }
+
+    return c.json(chat);
+  })
+  .delete("/:id", async (c) => {
+    const id = c.req.param("id");
+    const user = c.get("user");
+    const result = await removeChat({ userId: user.id, chatId: id });
+    return c.json({ chatId: result });
   });
-
-  if (!chat) {
-    return c.json(
-      {
-        message: "Error send message",
-      },
-      500
-    );
-  }
-
-  return c.json(chat);
-}).delete("/:id", async (c) => {
-  const id = c.req.param("id");
-  const user = c.get("user");
-  const result = await removeChat({ userId: user.id, chatId: id });
-  return c.json({ chatId: result });
-});
